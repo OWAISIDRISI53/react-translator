89b8e0f98e59aacfe24d3e6fe18e6ae33832d1f2
Owais Idrisi <79826730+OWAISIDRISI53@users.noreply.github.com>
2023-09-14 01:53:39 +0530

    Update Translate.jsx

--- src/Translate.jsx
+++ src/Translate.jsx
@@ -59,7 +59,7 @@ const Translate = () => {
     recognition.addEventListener("result", (e) => {
       console.log(e.results);
       const transcript = Array.from(e.results).map((result) =>
-        result[0].map((result) => result.transcript)
+        result.map((result) => result.transcript)
       console.log(transcript);
       setText(transcript);
     });
